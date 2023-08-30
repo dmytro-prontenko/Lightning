ae0cd0bcaf2b532fa1b79078acaa32ba3a162fc9
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2023-08-30 21:30:18 +0200

    Tree clean

--- src/index.js
+++ src/index.js
@@ -3,6 +3,3 @@ import './js/cockt-ingr-modal/modal-cocktail.js';
 import './js/classes/cocktailAPI.js';
 import './js/classes/render.js';
 import './js/elem-events.js';
-
-// @import url("./js/fav-cocktails.js");
-// @import url("./js/fav-ingredients.js");
--- src/js/fav-cocktails.js
+++ src/js/fav-cocktails.js
@@ -1,33 +1,3 @@
-import {get} from './fav-ingredients.js'
-
-import { refs } from './refs';
-import { renderModalCocktail } from './classes/render';
-import { saveToLocal, loadFromLocal } from './storage.js';
-
-refs.cocktailsList.addEventListener('click', onClick);
-
-function onClick(event) {
-  // Details button event
-  if (
-    event.target.nodeName === 'BUTTON' &&
-    event.target.className === 'learn-more'
-  ) {
-    console.log(event.target);
-
-    return;
-  }
-
-  // Favorite button event
-  if (
-    event.target.nodeName === 'BUTTON' &&
-    event.target.className === 'fav-btn'
-  ) {
-    saveToLocal('cocktails', event.target.closest('.cocktails-item').id);
-  }
-
-  console.log(localStorage.getItem('cocktails'));
-}
-
 
 
 
