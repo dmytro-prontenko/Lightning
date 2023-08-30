aba94fccb6c6fa602394de04e6c168212fd01400
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2023-08-30 11:59:12 +0200

    Added renderClass, coctailClass, fixed styles

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,9 @@
 export const refs = {
-    cocktailsList: document.querySelector('.cocktails-list'),
-    learnMoreBtn: document.querySelector('.learn-more'),
-    favBtn: document.querySelector('.fav-btn')
+  cocktailsList: document.querySelector('.cocktails-list'),
+  learnMoreBtn: document.querySelector('.learn-more'),
+  favBtn: document.querySelector('.fav-btn'),
+  modalCocktail: document.querySelector('.modal-cocktail'),
+  modalIngredient: document.querySelector('.js-modal-ingredient'),
+  modal: document.querySelector('[data-modal-cocktail]'),
+  listCocktail: document.querySelector('.modal-ingredients__list'),
 };
