f658897fd3ccf27878b9d8f882f2a453c6cbc1fa
pinkyjohnny <136741239+pinkyjohnny@users.noreply.github.com>
2023-08-31 14:36:08 +0300

    feature/hero-section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './js/modal.js';
 import './js/classes/cocktailAPI.js';
 import './js/classes/render.js';
-import './js/elem-events.js';
+import './js/hero.js';
+
+// @import url("./js/fav-cocktails.js");
+// @import url("./js/fav-ingredients.js");
