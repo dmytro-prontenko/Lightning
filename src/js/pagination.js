d48163e198fb7b52e147155ceab7768fe47e4738
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-09-03 02:38:21 +0300

    add logig prev next btn

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -11,53 +11,76 @@ let arrCocktail = [];
 refs.alphabet.addEventListener('click', onFilterSymbolClick);
 
 async function onFilterSymbolClick(event) {
-    arrCocktail = [];
-    refs.paginationContainer.innerHTML = "";
-    let pagObj={}
-    if (event.target.nodeName === 'LI') {
-        await listCocktails
-        .fetchCocktailByLetter(event.target.dataset.jsQuery)
-        .then(data => {
-        data.forEach(el => {arrCocktail.push(el)});
-            });
+  arrCocktail = [];
+  refs.paginationContainer.innerHTML = '';
+  let pagObj = {};
+  if (event.target.nodeName === 'LI') {
+    await listCocktails
+      .fetchCocktailByLetter(event.target.dataset.jsQuery)
+      .then(data => {
+        data.forEach(el => {
+          arrCocktail.push(el);
+        });
+      });
     event.target.closest('.custom-list').dataset.render = 'stop-render';
-    refs.cocktailsTitle.scrollIntoView({behavior: 'smooth'})
+    refs.cocktailsTitle.scrollIntoView({ behavior: 'smooth' });
     pagObj = createPaginationObject(arrCocktail, itemsPerPage);
-    Notiflix.Notify.info(`Found ${arrCocktail.length} cocktails!`)
-    
-        if (Object.keys(pagObj).length > 1) {
-            listRender.renderPaginationBtns(Object.keys(pagObj).length);
-            listRender.renderList(pagObj.page_1);
-        } else {
-            listRender.renderList(pagObj.page_1);
-        }
-    };
-    console.log(pagObj);
-};
+    Notiflix.Notify.info(`Found ${arrCocktail.length} cocktails!`);
 
-
-
-function createPaginationObject(values, itemsPerPage) {
-    const paginationObject = {};
-    const totalPages = Math.ceil(values.length / itemsPerPage);
-    for (let page = 1; page <= totalPages; page++) {
-        const startIdx = (page - 1) * itemsPerPage;
-        const endIdx = startIdx + itemsPerPage;
-        const pageValues = values.slice(startIdx, endIdx);
-        paginationObject['page_'+page] = pageValues;
+    if (Object.keys(pagObj).length > 1) {
+      listRender.renderPaginationBtns(Object.keys(pagObj).length);
+      listRender.renderList(pagObj.page_1);
+      refs.btnPaginationNext.classList.remove("is-hidden")
     }
-    return paginationObject;
+    else if(arrCocktail.length <= 9) {
+      listRender.renderList(pagObj.page_1);
+      refs.paginationContainer.innerHTML = `<button type="button" class="pagination-page" disabled id="page_${
+        Object.keys(pagObj).length
+            }">${Object.keys(pagObj).length}</button>`;
+        refs.btnPaginationNext.classList.add("is-hidden")
+      refs.btnPaginationPrev.classList.add("is-hidden")
+      }
+      
+      
+  }
+  refs.paginationContainer.addEventListener('click', onClickPageChanges);
+  function onClickPageChanges(event) {
+      listRender.renderList(pagObj[event.target.id]);
+
+    refs.cocktailsTitle.scrollIntoView({ behavior: 'smooth' });
+    refs.btnPaginationBlock.addEventListener('click', onPaginationNavClick);
+    function onPaginationNavClick(event) {
+      console.log(parseInt(event.target.id.match(/\d+/)));
+       let pageInt = parseInt(event.target.id.match(/\d+/));
+      if (pageInt > 1 && pageInt < Object.keys(pagObj).length) {
+          refs.btnPaginationPrev.classList.remove('is-hidden')
+          refs.btnPaginationNext.classList.remove('is-hidden')
+      }
+      else if (pageInt === 1) {
+          refs.btnPaginationNext.classList.remove('is-hidden') 
+          refs.btnPaginationPrev.classList.add('is-hidden')
+          
+      }
+      else if (pageInt === Object.keys(pagObj).length) {
+           refs.btnPaginationNext.classList.add('is-hidden') 
+          refs.btnPaginationPrev.classList.remove('is-hidden')
+      }
+      }
+      
+  }
 }
 
-
-
-
-
-
-
-
-
-
+function createPaginationObject(values, itemsPerPage) {
+  const paginationObject = {};
+  const totalPages = Math.ceil(values.length / itemsPerPage);
+  for (let page = 1; page <= totalPages; page++) {
+    const startIdx = (page - 1) * itemsPerPage;
+    const endIdx = startIdx + itemsPerPage;
+    const pageValues = values.slice(startIdx, endIdx);
+    paginationObject['page_' + page] = pageValues;
+  }
+  return paginationObject;
+}
 
 // `
 //         <li id=${card._id} class="cocktails-item">
