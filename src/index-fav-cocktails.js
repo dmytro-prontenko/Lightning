c2d8a2a09b62e42e2018b9ca83f09dc7c17c880b
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-09-04 10:20:59 +0300

    1

--- src/index-fav-cocktails.js
+++ src/index-fav-cocktails.js
@@ -2,9 +2,9 @@ import './js/fav-cocktails';
 import './js/elem-events.js';
 import './js/button-to-up.js'
 import './js/dark-theme.js';
+import './js/header.js';
 // import './js/onLoadPage.js';
 // import './js/classes/cocktailAPI.js';
 // import './js/classes/render.js';
-// import './js/header.js';
 // import './js/classes/drop-down-search.js';
-// import './js/pagination.js';
+
--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,6 @@
 const headerLinkFav = document.querySelector('.header-nav-link-fav');
 const favMenu = document.querySelector('.header-favorite-menu');
-import { gsap } from 'gsap';
+
 
 headerLinkFav.addEventListener('click', onClick);
 
